53f579f31a042ec699400cf4202a1b4886cb3d51
imtinannaqvi <imtinannadeem176@gmail.com>
2026-06-06 16:37:48 +0500

    first commit

---
+++ app/Components/Tech.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Tech = () => {
+  return (
+    <>
+     <div>
+      <div>
+        <h1 className='text-white text-3xl font-semibold'>
+            Technology at your service!
+        </h1>
+        <p>
+            Sed convallis, lorem quis viverra dictum, ex diam cursus justo, ut vehicula lacus leo vel ante. Sed at egestas lorem.
+Etiam lobortis metus justo, tempor commodo risus cursus sit amet. Donec at lacinia magna.
+        </p>
+        <img src="/app-store-1.png" alt="" />
+        <img src="/app-store2.webp" alt="" />
+      </div>
+      <div>
+        <img src="/device-1.png" alt="" />
+      </div>
+    </div>
+    </>
+   
+  )
+}
+
+export default Tech
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Tech from "./Components/Tech";
+import Footer from "./Components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Tech/>
+      <Footer/>
+    </>
+  );
+}
